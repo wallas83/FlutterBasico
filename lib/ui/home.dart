03ba7879fd6fbcce961570e0ad510f70064d3420
wilson <whuanca06@gmail.com>
2019-06-24 15:06:24 -0400

    adding radio buttons

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -9,6 +9,15 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  int radioValue = 0;
+
+  void handleRadioValueChanged( int value){
+      setState(() {
+        radioValue = value;
+
+        print(radioValue);
+      });
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -38,9 +47,56 @@ class HomeState extends State<Home> {
                     controller: null,
                     keyboardType: TextInputType.number,
                     decoration: new InputDecoration(
-                        labelText: 'Your Weight on Earth',
-                        hintText: 'In Pounds',
-                        icon: new Icon(Icons.person_outline)),
+                      labelText: 'Your Weight on Earth',
+                      hintText: 'In Pounds',
+                      icon: new Icon(Icons.person_outline),
+                    ),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.all(5.0),
+                  ),
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      //radio buttons
+                      new Radio<int>(
+                        activeColor: Colors.brown,
+                        value: 0,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChanged),
+                      new Text(
+                        "pluto",
+                        style: new TextStyle(color: Colors.white30),
+                      ),
+
+                      new Radio<int>(
+                        activeColor: Colors.redAccent,
+                        value: 1,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChanged),
+                      new Text(
+                        "mars",
+                        style: new TextStyle(color: Colors.white30),
+                      ),
+                      new Radio<int>(
+                        activeColor: Colors.yellowAccent,
+                        value: 2,
+                        groupValue: radioValue,
+                        onChanged: handleRadioValueChanged),
+                      new Text(
+                        "venus",
+                        style: new TextStyle(color: Colors.white30),
+                      ),
+                    ],
+                  ),
+                  new Padding(padding: new EdgeInsets.all(15.6)),
+                  //result text
+                  new Text(
+                    "hello there",
+                    style: new TextStyle(
+                      color: Colors.white30,
+                      fontWeight: FontWeight.w900
+                    ),
                   )
                 ],
               ),
